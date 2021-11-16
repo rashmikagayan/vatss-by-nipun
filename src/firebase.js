022c980655ddddcbce79a635b47fc6362663349a
Rashmika Gayan Perera <39581401+rashmikagayan@users.noreply.github.com>
2021-11-16 22:14:48 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-    apiKey: "AIzaSyBFo6xn3KwZWF7x4_-c2CiGDK5Zhp-MY9E",
+    apiKey: "NULL",
     authDomain: "vatss-5cfb5.firebaseapp.com",
-    databaseURL: "https://vatss-5cfb5-default-rtdb.asia-southeast1.firebasedatabase.app",
+    databaseURL: "NULL",
     projectId: "vatss-5cfb5",
     storageBucket: "vatss-5cfb5.appspot.com",
-    messagingSenderId: "98707858245",
-    appId: "1:98707858245:web:e3e6ae69f12d9cd941d42c"
+    messagingSenderId: "NULL",
+    appId: "NULL"
 };
     
 
